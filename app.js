c58e66d834ebe3885f6564ab9b8f044b9d04209b
gagankaul <63837949+gagankaul@users.noreply.github.com>
2021-06-30 13:20:20 -0400

    updated

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-var todos = [];
+//An example of functional programming in Javascript
 
-function addTodo() {
-  var newTodo = prompt("Please add a new Todo: ");
-  todos.push(newTodo);
+var todos = [
+  {todoText:"", completed:false},
+  {todoText:"", completed:false},
+  {todoText:"", completed:false}
+];
+
+function displayTodos() {
+  console.log("My Todos: ", todos);
+}
+
+//Assuming that all new Todos will have the default completed status as false
+
+function addTodo(initialTodoText) {
+  todos.push({todoText: initialTodoText,
+    completed: false});
   console.log(todos);
+}
+
+function changeTodo(position, newTodoText) {
+  todos[position].todoText = newTodoText;
+  console.log(todos)
+}
+
+function deleteTodo(position) {
+  todos.splice(position, 1);
+  displayTodos();
+}
+
+function toggleTodo(position) {
+  if (todos[position].completed === false) {
+    todos[position].completed = true;
+  } else {
+    todos[position].completed = false;
+  }
 }
