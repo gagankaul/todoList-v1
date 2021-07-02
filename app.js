5539ed2bfb8b6b238f70e51e9b859e2ba5b6139c
gagankaul <63837949+gagankaul@users.noreply.github.com>
2021-07-02 14:37:06 -0400

    Added toggleAll() function to check/uncheck all todos

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //An example of functional programming in Javascript
 
 var todos = [
-  {todoText:"", completed:false},
-  {todoText:"", completed:false},
-  {todoText:"", completed:false}
+  {todoText:"item 1", completed:false},
+  {todoText:"item 2", completed:false},
+  {todoText:"item 3", completed:false}
 ];
 
 function displayTodos() {
-  console.log("My Todos: ", todos);
+  for (var i = 0; i < todos.length; i++) {
+    console.log(todos[i]);
+  }
 }
 
 //Assumption: All new Todos will have the default completed status as false.
@@ -15,12 +17,12 @@ function displayTodos() {
 function addTodo(initialTodoText) {
   todos.push({todoText: initialTodoText,
     completed: false});
-  console.log(todos);
+  displayTodos();
 }
 
 function changeTodo(position, newTodoText) {
   todos[position].todoText = newTodoText;
-  console.log(todos)
+  displayTodos();
 }
 
 function deleteTodo(position) {
@@ -34,4 +36,25 @@ function toggleTodo(position) {
   } else {
     todos[position].completed = false;
   }
+  displayTodos();
+}
+
+function toggleAll() {
+  
+  completedTodos = 0;
+
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i].completed === true) {
+      completedTodos++;
+    }
+  }
+
+  for (var i = 0; i < todos.length; i++) {
+    if (completedTodos === todos.length) {
+      todos[i].completed = false;  
+  } else {
+    todos[i].completed = true;
+  }
+  }
+  displayTodos();
 }
