9f068a601679399b751e32dbf70008bbd6c4252e
gagankaul <63837949+gagankaul@users.noreply.github.com>
2021-07-03 21:39:01 -0400

    Added a Toggle All button and functionality

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 //An example of functional programming in Javascript
 
+//Creating a todos object
 var todos = [
   {todoText:"item 1", completed:false},
   {todoText:"item 2", completed:false},
   {todoText:"item 3", completed:false}
 ];
 
+//Display Todos function
 function displayTodos() {
   for (var i = 0; i < todos.length; i++) {
     console.log(todos[i]);
   }
 }
 
+//Add a Todo function
 //Assumption: All new Todos will have the default completed status as false.
-
 function addTodo(initialTodoText) {
   todos.push({todoText: initialTodoText,
     completed: false});
   displayTodos();
 }
 
+//Edit a Todo
 function changeTodo(position, newTodoText) {
   todos[position].todoText = newTodoText;
   displayTodos();
 }
 
+//Delete a Todo
 function deleteTodo(position) {
   todos.splice(position, 1);
   displayTodos();
 }
 
+//Toggle a single Todo
 function toggleTodo(position) {
   if (todos[position].completed === false) {
     todos[position].completed = true;
@@ -39,6 +44,7 @@ function toggleTodo(position) {
   displayTodos();
 }
 
+//Toggle All Function
 function toggleAll() {
   
   var completedTodos = 0;
@@ -58,3 +64,12 @@ function toggleAll() {
   }
   displayTodos();
 }
+
+//Display Todos Button Functionality
+var displayTodosButton = document.getElementById("display-todos-button");
+displayTodosButton.addEventListener("click", displayTodos);
+
+//Toggle All Button Functionality
+var toggleAllbutton = document.getElementById("toggle-all-button");
+toggleAllbutton.addEventListener("click", toggleAll);
+
